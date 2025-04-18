79fd83746ae5b7dec890a4bf66f0076a55a2ca40
wulpine <wulpine@proton.me>
2025-04-18 13:12:30 +0300

    Decrease miss penalty

--- osu.Game.Rulesets.Catch/Difficulty/CatchPerformanceCalculator.cs
+++ osu.Game.Rulesets.Catch/Difficulty/CatchPerformanceCalculator.cs
@@ -49,11 +49,11 @@ namespace osu.Game.Rulesets.Catch.Difficulty
 
             if (numMiss > 0)
             {
-                double missPenalty = 0.97 / (numMiss / (4 * Math.Pow(Math.Log(catchAttributes.MovementDifficultStrainCount), 0.94)) + 1);
+                double missPenalty = 0.97 / (numMiss / (5 * Math.Pow(Math.Log(catchAttributes.MovementDifficultStrainCount), 1.07)) + 1);
                 double worstCaseMaxCombo = Math.Ceiling((double)(catchAttributes.MaxCombo - numMiss) / (numMiss + 1));
                 double comboRatio = catchAttributes.MaxCombo - numMiss - worstCaseMaxCombo == 0 ? 0 : (score.MaxCombo - worstCaseMaxCombo) / (catchAttributes.MaxCombo - numMiss - worstCaseMaxCombo);
                 double comboBonus = comboRatio <= 0.780807 ? Math.Pow(comboRatio, 0.8) / 1.4 : 1 / (1 + Math.Pow(0.64 * (1 - comboRatio) / (0.36 * comboRatio), 0.5));
-                value *= missPenalty + 0.7 * comboBonus * (1 - missPenalty);
+                value *= missPenalty + 0.65 * comboBonus * (1 - missPenalty);
             }
 
             var difficulty = score.BeatmapInfo!.Difficulty.Clone();
